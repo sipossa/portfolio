41eb4eab1f90e8afbd06fdfb103730370f49fd97
sipossa <87564335+sipossa@users.noreply.github.com>
2021-07-24 17:01:15 +0900

    Add files via upload

---
+++ 002/pf04/tail.php
@@ -0,0 +1,13 @@
+<footer>
+    <div>Ruscello, 대표자명: 조재호, 사업자등록번호 : 652-03-01169, 대표전화: 055-681-2032, 이메일: skel87@naver.com<br>주소 : 경남 거제시 일운면 1861 (망치리), 구주소: 경남 거제시 일운면 망치리 35</div>
+    <div>
+        <li><a href="">통신판매업확인</a></li>
+        <li><a href="">저작권</a></li>
+        <li><a href="">이용약관</a></li>
+        <li><a href="">개인정보처리방침</a></li>
+        <li><a href="">INSTAGRAM</a></li>
+    </div>
+    <p>COPYRIGHT© 2020 Ruscello. All rights reserved. Designed by sooooojin</p>
+</footer>
+</body>
+</html>
